045a9a9f70f574ae7515d7dd33d95d1b5de0df30
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2023-05-12 11:57:57 +0300

    task 8-9

--- js/task-08.js
+++ js/task-08.js
@@ -1,10 +1,21 @@
-const user = {
-    email: '',
-    password: ''
-}
-
 const formEl = document.querySelector('.login-form');
 const subEl = formEl.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log(event);
+
+    const emailEl = formEl.elements.email;
+    const passwordEl = formEl.elements.password;
+
+    if (!emailEl.value || !passwordEl.value) {
+        alert('Всі поля повинні бути заповнені!');
+        return;
+    }
+
+    const user = {
+        email: emailEl.value,
+        password: passwordEl.value
+    }
+    console.log(user);
+
+    formEl.reset();
 })
+
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+const btnEl = document.querySelector('.change-color');
+
+btnEl.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  spanEl.textContent = color;
+})
